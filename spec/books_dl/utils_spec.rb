f9626038f76666478cd7501d1c5779476f14e941
David Liu <msglight4874@gmail.com>
2019-03-01 16:28:08 +0800

    add BooksDL::Utils.hex_string_to_byte examples

---
+++ spec/books_dl/utils_spec.rb
@@ -0,0 +1,20 @@
+describe BooksDL::Utils do
+  describe '.hexStringToByte' do
+    let(:hex) { 'ABCDEFG987654321' }
+    let(:bytes_array) { [171, 205, 239, 0, 135, 101, 67, 33] }
+
+    it 'return empty array when input is nil' do
+      return_value = described_class.hex_string_to_byte(nil)
+
+      expect(return_value).to be_a Array
+      expect(return_value).to be_empty
+    end
+
+    it 'return byte array correctly' do
+      return_value = described_class.hex_string_to_byte(hex)
+
+      expect(return_value).to be_a Array
+      expect(return_value).to match_array(bytes_array)
+    end
+  end
+end
