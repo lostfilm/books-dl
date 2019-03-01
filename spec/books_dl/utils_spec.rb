8c4cfb80e992263c1a2e79c1f1edcb08bdf8edcc
David Liu <msglight4874@gmail.com>
2019-03-01 17:12:38 +0800

    add BooksDL::Utils.get_real example

--- spec/books_dl/utils_spec.rb
+++ spec/books_dl/utils_spec.rb
@@ -14,7 +14,19 @@ describe BooksDL::Utils do
       return_value = described_class.hex_string_to_byte(hex)
 
       expect(return_value).to be_a Array
-      expect(return_value).to eq(bytes_array)
+      expect(return_value).to eq bytes_array
+    end
+  end
+
+  describe '.get_real' do
+    let(:url) { 'https://streaming-ebook.books.com.tw/V1.0/Streaming/book/DD0CB3/952170/OEBPS/content.opf' }
+    let(:decode) { [101, 87, 67, 247, 38, 70, 65, 140, 139, 83, 14, 193, 211, 197, 38, 225, 48, 35, 79, 108, 47, 47, 191, 253, 44, 205, 93, 130, 226, 96, 203, 82] }
+
+    it 'return decode bytes array' do
+      return_value = described_class.get_real(url)
+
+      expect(return_value).to be_a Array
+      expect(return_value).to eq decode
     end
   end
 end
