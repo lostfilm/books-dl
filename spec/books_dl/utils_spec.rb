98715ca1eaadf1af9de129e8d97d466fef3d12cc
David Liu <msglight4874@gmail.com>
2019-03-01 19:44:30 +0800

    add BooksDL::Utils#xor_decoder examples

--- spec/books_dl/utils_spec.rb
+++ spec/books_dl/utils_spec.rb
@@ -23,9 +23,9 @@ describe BooksDL::Utils do
 
   describe '#get_real' do
     let(:url) { 'https://streaming-ebook.books.com.tw/V1.0/Streaming/book/DD0CB3/952170/OEBPS/content.opf' }
-    let(:url2) { 'https://streaming-ebook.books.com.tw/V1.0/Streaming/book/DD0CB3/952170/OEBPS/container.xml' }
+    let(:url2) { 'https://streaming-ebook.books.com.tw/V1.0/Streaming/book/DD0CB3/952170/META-INF/container.xml' }
     let(:decode) { [101, 87, 67, 247, 38, 70, 65, 140, 139, 83, 14, 193, 211, 197, 38, 225, 48, 35, 79, 108, 47, 47, 191, 253, 44, 205, 93, 130, 226, 96, 203, 82] }
-    let(:decode2) { [51, 93, 164, 46, 149, 231, 224, 178, 109, 229, 152, 16, 210, 212, 27, 208, 10, 225, 231, 49, 57, 84, 190, 177, 112, 220, 145, 15, 9, 97, 213, 206] }
+    let(:decode2) { [125, 142, 71, 182, 184, 151, 206, 229, 41, 53, 224, 96, 131, 141, 200, 22, 28, 118, 85, 249, 243, 74, 28, 193, 218, 219, 4, 243, 201, 221, 108, 117] }
 
     it 'return decode bytes array' do
       return_value = subject.get_real(url)
@@ -39,6 +39,28 @@ describe BooksDL::Utils do
 
       expect(return_value).to be_a Array
       expect(return_value).to eq decode2
+    end
+  end
+
+  describe '#xor_decoder' do
+    let(:url) { 'https://streaming-ebook.books.com.tw/V1.0/Streaming/book/DD0CB3/952170/OEBPS/content.opf' }
+    let(:encrypted_content_opf) { file_fixture('encrypted_content.opf').read }
+    let(:content_opf) { file_fixture('content.opf').read }
+
+    let(:url2) { 'https://streaming-ebook.books.com.tw/V1.0/Streaming/book/DD0CB3/952170/META-INF/container.xml' }
+    let(:encrypted_container_xml) { file_fixture('encrypted_container.xml').read }
+    let(:container_xml) { file_fixture('container.xml').read }
+
+    it 'return decoded content opf' do
+      return_value = subject.xor_decoder(url, encrypted_content_opf)
+
+      expect(return_value).to eq content_opf
+    end
+
+    it 'return decoded container.xml' do
+      return_value = subject.xor_decoder(url2, encrypted_container_xml)
+
+      expect(return_value).to eq container_xml
     end
   end
 end
